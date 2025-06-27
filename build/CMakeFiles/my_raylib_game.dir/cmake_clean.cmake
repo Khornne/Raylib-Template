509b9bbeedca4b99c8e5cad26a69b9ed2b6efbd8
Khornne <maurice.pattinsonn@gmail.com>
2025-06-27 12:23:22 +0100

    Initial Commit

---
+++ build/CMakeFiles/my_raylib_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_raylib_game.dir/link.d"
+  "CMakeFiles/my_raylib_game.dir/src/main.cpp.o"
+  "CMakeFiles/my_raylib_game.dir/src/main.cpp.o.d"
+  "my_raylib_game"
+  "my_raylib_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_raylib_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
